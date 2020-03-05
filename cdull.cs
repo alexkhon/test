5d0acbce6ed02ede4716a41a179035503fb254d8
Peter Hon <peter.hon@gmail.com>
2020-03-05 16:31:40 -0500

    my cdull stuff

---
+++ cdull.cs
@@ -0,0 +1,11 @@
+// this is a comment
+public class history{
+  public int ClassId { get; set; }
+  public string ClassName { get; set; }
+
+}
+
+public string EndOfTheWorld(string Date, string Time){
+  string Now = "today";
+  return(Now);
+}
